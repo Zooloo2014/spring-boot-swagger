8be0b277cf2abf6fcdf73e016adfdbd660b68bbf
Brent Engelbrecht <bengelbrecht@mhg.co.za>
2018-08-30 13:43:42 +0100

    Initial commit

---
+++ src/main/java/com/example/springbootswagger/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.example.springbootswagger.entity;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String address;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

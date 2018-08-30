08a5fe3ffd75485255c34f6619577f1441041f7e
Brent Engelbrecht <bengelbrecht@mhg.co.za>
2018-08-30 13:50:14 +0100

    Fix packages

--- src/main/java/com/example/springbootswagger/controller/CustomerController.java
+++ src/main/java/com/example/springbootswagger/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.example.springbootswagger;
+package com.example.springbootswagger.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
